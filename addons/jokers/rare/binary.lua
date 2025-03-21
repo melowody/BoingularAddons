e524c928a5dcb6b145d29bf2bdcc79a3b56011fa
moonlightmari <lemoncrescent7@gmail.com>
2025-03-21 19:27:20 -0400

    Binary Joker done

---
+++ addons/jokers/rare/binary.lua
@@ -0,0 +1,49 @@
+SMODS.Joker{
+    key = "binary",
+    loc_txt = {
+        name = "Binary Joker",
+        text = {
+            "This {C:attention}Joker{} alternates",
+            "{C:white,X:mult}X#2#{} {C:mult}Mult{} and {C:white,X:chips}X#1#{} {C:chips}Chips{}",
+            "on every hand played"
+              }
+    },
+    rarity = 3,
+    atlas = "BoingularJokers",
+    pos = {
+        x = 9,
+        y = 0
+    },
+    config = {
+        extra = {
+            chips = 4,
+            mult = 2,
+            decide = 1
+        },
+    },
+    loc_vars = function(self, info_queue, card)
+        return {
+                vars = {
+                    card.ability.extra.chips,
+                    card.ability.extra.mult
+                }
+        }
+    end,
+    cost = 8,
+    eternal_compat = true,
+    perishable_compat = true,
+    blueprint_compat = true,
+    calculate = function (self, card, context)
+        if context.cardarea == G.jokers and context.joker_main then
+            card.ability.extra.decide = card.ability.extra.decide + 1
+          if card.ability.extra.decide % 2 == 0 then return {
+                colour = G.C.MULT,
+                xmult = card.ability.extra.mult
+            } elseif card.ability.extra.decide % 2 == 1 then return {
+                colour = G.C.CHIPS,
+                xchips = card.ability.extra.chips
+            }
+            end
+         end
+    end
+}
