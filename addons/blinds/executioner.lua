b021b23dd274d0d88bf899e65eb09c45827d7c44
moonlightmari <lemoncrescent7@gmail.com>
2025-03-21 02:14:10 -0400

    Executioner bungle

--- addons/blinds/executioner.lua
+++ addons/blinds/executioner.lua
@@ -1,12 +1,30 @@
+local old_end_round = end_round
+function end_round()
+    old_end_round()
+end
 SMODS.Blind {
     key = "executioner_blind",
     loc_txt = {
         name="The Executioner",
         text={
-            "Playing a [most played hand]",
+            "Playing a #1#",
             "sets chips to 0"
         }
     },
+    loc_vars = function (self)
+        return {
+            vars = {
+                (G.GAME and G.GAME.current_round and G.GAME.current_round.most_played_poker_hand) and localize(G.GAME.current_round.most_played_poker_hand, "poker_hands") or "[most played hand]"
+            }
+        }
+    end,
+    collection_loc_vars = function (self)
+        return {
+            vars = {
+                "[most played hand]"
+            }
+        }
+    end,
     atlas = "BoingularBlinds",
     pos = { x = 0, y = 2 },
     mult = 2,
