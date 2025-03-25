77bbefa95e17ac0319f67bef52b3daa636e1ce64
Melody <melodydevofficial@gmail.com>
2025-03-25 03:16:35 -0400

    Fixed Six Fingers and unjankified it

--- addons/jokers/rare/six_fingers.lua
+++ addons/jokers/rare/six_fingers.lua
@@ -1,16 +1,3 @@
-local can_play_old = G.FUNCS.can_play
-
-G.FUNCS.can_play = function(e)
-    can_play_old(e)
-    if #G.hand.highlighted <= 0 or G.GAME.blind.block_play then 
-        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
-        e.config.button = nil
-    else
-        e.config.colour = G.C.BLUE
-        e.config.button = 'play_cards_from_highlighted'
-    end
-end
-
 SMODS.Joker {
     key = "six_fingers",
     loc_txt = {
@@ -28,14 +15,20 @@ SMODS.Joker {
     eternal_compat = true,
     perishable_compat = true,
     blueprint_compat = false,
-    calculate = function(self, card, context)
-        if context.setting_blind then
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    G.hand.config.highlighted_limit = 6
-                    return true
-                end
-            }))
-        end
+    add_to_deck = function(self, card, from_debuff)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.hand.config.highlighted_limit = G.hand.config.highlighted_limit + 1
+                return true
+            end
+        }))
+    end,
+    remove_from_deck = function (self, card, from_debuff)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.hand.config.highlighted_limit = G.hand.config.highlighted_limit - 1
+                return true
+            end
+        }))
     end
 }
