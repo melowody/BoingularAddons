4725d49ebaf23da39a20fd04e295daa036841448
moonlightmari <lemoncrescent7@gmail.com>
2025-03-27 21:34:06 -0400

    WLM bones + art

---
+++ addons/jokers/2_uncommon/whole_lotta_mult.lua
@@ -0,0 +1,24 @@
+SMODS.Joker {
+    key = "whole_lotta",
+    loc_txt = {
+        name = "Whole Lotta Mult",
+        text = {
+            "yada yada"
+        }
+    },
+    rarity = 1,
+    atlas = "BoingularJokers",
+    pos = {x = 4, y = 1},
+    cost = 6,
+    loc_vars = function (self, info_queue, card)
+        
+    end,
+    eternal_compat = true,
+    perishable_compat = true,
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+            if context.individual and context.joker_main then
+                
+            end
+    end
+}
