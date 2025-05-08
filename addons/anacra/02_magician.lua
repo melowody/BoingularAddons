5e45d03b01a99a3169b8fdea8c52f500e2fd39f9
moonlightmari <lemoncrescent7@gmail.com>
2025-05-07 20:57:43 -0400

    anacra 2/4/6/8 done

--- addons/anacra/02_magician.lua
+++ addons/anacra/02_magician.lua
@@ -3,12 +3,41 @@ SMODS.Consumable {
     loc_txt = {
         name = "The Magician?",
         text = {
-            "Enhances {C:attention}1{} selected",
-            "card into a",
-            "{C:attention}Singed Card"
+            "Enhances up to {C:attention}2{}",
+            "selected cards into",
+            "{C:attention}Singed Cards"
         }
     },
     set = "BoingularAnacra",
     atlas = "BoingularAnacra",
-    pos = {x = 1, y = 0}
+    pos = {x = 1, y = 0},
+
+    can_use = function(self, card)
+    if G.hand and ((#G.hand.highlighted == 2) or (#G.hand.highlighted == 1)) then
+        return true
+        end
+    end,
+
+    use = function(self, card)
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+            play_sound('tarot1')
+            card:juice_up(0.3, 0.5)
+            return true end }))
+        for i=1, #G.hand.highlighted do
+            local percent = 1.15 - (i-0.999)/(#G.hand.highlighted-0.998)*0.3
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('card1', percent);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
+        end
+        delay(0.2)
+        for i=1, #G.hand.highlighted do
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1,func = function() G.hand.highlighted[i]:set_ability("m_boingular_singed");return true end }))
+        end 
+        for i=1, #G.hand.highlighted do
+            local percent = 0.85 + (i-0.999)/(#G.hand.highlighted-0.998)*0.3
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('tarot2', percent, 0.6);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
+        end
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
+        delay(0.5)
+    end
+
+    
 }
--- addons/anacra/04_empress.lua
+++ addons/anacra/04_empress.lua
@@ -10,5 +10,25 @@ SMODS.Consumable {
     },
     set = "BoingularAnacra",
     atlas = "BoingularAnacra",
-    pos = {x = 3, y = 0}
+    pos = {x = 3, y = 0},
+
+    can_use = function(self, card)
+    if G.hand and (#G.hand.highlighted == 1) then
+        return true
+        end
+    end,
+
+    use = function(self, card)
+        G.E_MANAGER:add_event(Event({func = function()
+            play_sound('tarot1')
+            card:juice_up(0.3, 0.5)
+            return true end }))
+        
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1,func = function()
+            G.hand.highlighted[1]:set_seal("boingular_orange_seal", nil, true)
+            return true end }))
+        
+        delay(0.5)
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
+    end
 }
