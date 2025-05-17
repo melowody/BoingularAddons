db99e2d9883e5953a884406149328441c0673fa9
moonlightmari <lemoncrescent7@gmail.com>
2025-05-17 17:25:31 -0400

    holy fucking shgit

--- addons/tags/rune tags/06_ansuz.lua
+++ addons/tags/rune tags/06_ansuz.lua
@@ -13,8 +13,29 @@ SMODS.Tag {
     atlas = "BoingularTags",
     pos = {x = 0, y = 1},
     config = {
-        type = ""
+        type = "boinglar_update_shop"
     },
     apply = function (self, tag, context)
-    end
+        print(context.type)
+        if context.type == 'shop_final_pass' or (context.type == 'immediate' and not not G.shop_jokers and not not G.shop_booster) then
+                if G.shop and not G.shop_free then
+                    G.GAME.shop_free = true
+                    tag:yep('Ansuz!', G.C.BOINGULAR_LIFE_RUNE ,function()
+                        if G.shop_jokers and G.shop_booster then
+                            for k, v in pairs(G.shop_jokers.cards) do
+                                v.ability.couponed = true
+                                v:set_cost()
+                            end
+                            for k, v in pairs(G.shop_booster.cards) do
+                                v.ability.couponed = true
+                                v:set_cost()
+                            end
+                        end
+                        return true
+                    end)
+                end
+                tag.triggered = true
+                return true
+            end
+            end
 }
